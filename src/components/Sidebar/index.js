3ed5b1d8b14129738d9f46aaeca394dbfa072a02
Alex Sicart <hello@alexsicart.com>
2018-08-23 11:55:36 +0200

    style: overpass mono

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
-class Index extends React.Component {
+class Index extends Component {
   render() {
     return (
       <div>
-          <Sidebar as={Menu} animation='overlay' style icon='labeled' vertical visible width='wide'>
+          <Sidebar as={Menu} animation='overlay'  icon='labeled' vertical visible width='wide'>
             <Menu.Item as='a'>
-              <Icon name='home' />
-              Home
+              <h1>Shasta</h1>
             </Menu.Item>
             <Menu.Item as='a'>
               <Icon name='gamepad' />
