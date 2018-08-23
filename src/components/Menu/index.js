0c856e7ec437274398f10738481baad32563f97b
Alex Sicart <hello@alexsicart.com>
2018-08-23 11:01:08 +0200

    fix: menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import { Menu, Button } from 'semantic-ui-react'
 
-import './App.css';
-
-class Menu extends React.Component {
+class Index extends Component {
   render() {
     return (
+      // Menu with Bulma-React.
       <div>
-
+        <Menu size='small'>
+          <Menu.Item>
+            <span>Shasta</span>
+          </Menu.Item>
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <p>Private Network: QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn</p>
+            </Menu.Item>
+            <Menu.Item>
+              <Button primary>Sign Up</Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
       </div>
     );
   }
 }
 
-export default Menu;
+export default Index;
