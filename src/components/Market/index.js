20da62ac30e573bc612040a9987e14dd35a5267d
Alex Sicart <hello@alexsicart.com>
2018-08-23 13:13:16 +0200

    feat: router integration

---
+++ src/components/Market/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Menu, Button } from 'semantic-ui-react'
+
+class Index extends Component {
+  render() {
+    return (
+      // Menu with Bulma-React.
+      <div>
+<h1>hey</h1>
+      </div>
+    );
+  }
+}
+
+export default Index;
--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 
-import Menu from './Menu/index';
-import Sidebar from './Sidebar/index';
+import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+import Tab from './Tab/index';
 import Home from './Home/index';
+import Market from './Market/index';
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Menu></Menu>
-        <Sidebar></Sidebar>
-        <Home></Home>
-      </div>
+      <Router>
+        <div>
+          <Tab></Tab>
+          <Sidebar as={Menu} animation='overlay'  icon='labeled' vertical visible width='wide'>
+            <Menu.Item as='a'>
+              <h2>Shasta</h2>
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Link to="/market">Market</Link>
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='user' />
+              Network
+            </Menu.Item>
+          </Sidebar>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/market" component={Market} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
