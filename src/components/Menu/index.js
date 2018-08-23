5113545804a246f66a53f00f19cd8beb791ad869
Alex Sicart <hello@alexsicart.com>
2018-08-23 10:59:15 +0200

    feat: menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Menu;
