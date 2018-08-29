967f6e659f7f9d7da64463f4c5184e13248a067d
Unknown <ignasi.ramos@alu-etsetb.upc.edu>
2018-08-29 13:20:33 +0200

    feat: ipfs storing implementation

--- src/components/CreateUser/index.js
+++ src/components/CreateUser/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Form} from 'semantic-ui-react'
 
-
 class Index extends Component {
 
     constructor(props) {
@@ -20,25 +19,36 @@ class Index extends Component {
 
         console.log("Creating user " + this.state.username);
         
-        var User = this.props.user;
         var username = this.state.username;
+        var userJson = {
+            username: username
+        }
+
+        var ipfsHash = '';
+        var contract = this.props.userContract;
         var account = this.props.account;
-        var ipfsHash = 'not-available';
 
-        
-        User.deployed().then(function(contractInstance) {
-            contractInstance.createUser(username, ipfsHash, {gas: 200000, from: account}).then(function(success) {
-            if(success) {
-            console.log('created user ' + username + 'on ethereum!');
-
-            } else {
-            console.log('error creating user on ethereum. Maybe the user name already exists or you already have a user.');
-            }
-           }).catch(function(e) {
-            console.log('error creating user:', username, ':', e);
-            });
+        this.props.ipfs.add([Buffer.from(JSON.stringify(userJson))], function(err, res) {
             
-           });
+            if (err) throw err;
+            ipfsHash = res[0].hash;
+            console.log("ipfs hash: ", ipfsHash);
+
+            contract.deployed().then(function(contractInstance) {
+                contractInstance.createUser(username, ipfsHash, {gas: 400000, from: account}).then(function(success) {
+                if(success) {
+                console.log('created user ' + username + ' on ethereum!');
+
+                } else {
+                console.log('error creating user on ethereum. Maybe the user name already exists or you already have a user.');
+                }
+            }).catch(function(e) {
+                console.log('error creating user:', username, ':', e);
+                });
+                
+            });
+        });
+        
     }
 
     updateInput(event){
