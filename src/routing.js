d895a3b3737dee967266a255d23ad406ecaf695e
Unknown <ignasirv@hotmail.com>
2018-09-06 13:56:16 +0200

    feat: new button to build a demo environment

--- src/routing.js
+++ src/routing.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import { Image, Menu, Sidebar } from 'semantic-ui-react'
+import { Image, Menu, Sidebar, Button } from 'semantic-ui-react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
 
 import Tab from './components/Tab/Tab';
 import Home from './components/Home/Home';
@@ -14,6 +13,76 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.createDemo = this.createDemo.bind(this);
+
+  }
+
+  async createDemo() {
+
+    var faker = require('faker');
+
+    const bid = {
+      value: 40,
+      date: Date.now(),
+      firstName: "Vitalik",
+      lastName: "Buterin",
+      country: "Russia",
+      marketer: "The Marketer",
+      source: "Nuclear"
+    }
+
+
+    this.props.userJson.contracts.push(bid);
+
+    const res = await this.props.ipfs.add([Buffer.from(JSON.stringify(this.props.userJson))]);
+
+    let ipfsH = res[0].hash;
+    let contractInstance = await this.props.contract.deployed();
+
+    await contractInstance.createBid(bid.value, ipfsH, { gas: 400000, from: this.props.address, value: bid.value })
+
+    const bid2 = {
+      value: 20,
+      date: Date.now(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      country: faker.address.country(),
+      marketer: "The Marketer",
+      source: "Solar"
+    }
+    this.props.userJson.contracts.push(bid2);
+    const res2 = await this.props.ipfs.add([Buffer.from(JSON.stringify(this.props.userJson))]);
+
+    ipfsH = res2[0].hash;
+
+    await contractInstance.createBid(bid2.value, ipfsH, { gas: 400000, from: this.props.address, value: bid2.value })
+
+    // Get the SharedMap.sol instance
+    const sharedMapInstance = await this.props.sharedMapContract.deployed();
+
+    // Generate the location object, will be saved later in JSON.
+    const locationObject = {
+      chargerName: "charger-" + faker.random.number(),
+      chargerStatus: "open",
+      latitude: "36.70",
+      longitude: "-4.457",
+      providerSource: "Solar",
+      address: faker.address.streetAddress(),
+    }
+    const priceBG = "3";
+
+    // Upload to IPFS and receive response
+    const ipfsResponse = await this.props.ipfs.add(Buffer.from(JSON.stringify(locationObject)));
+    const ipfsHash = ipfsResponse[0].hash;
+    // Estimate gas
+    const estimatedGas = await sharedMapInstance.addLocation.estimateGas(priceBG, ipfsHash, { from: this.props.address });
+    // Send a transaction to addLocation method.
+    await sharedMapInstance.addLocation(priceBG, ipfsHash, { gas: estimatedGas, from: this.props.address })  //Call the transaction
+
+  }
 
   render() {
     const {
@@ -27,12 +96,12 @@ class App extends React.Component {
         <Router>
           <div>
             <Tab web3={web3} account={account} balance={balance}></Tab>
-            <Sidebar as={Menu} animation='overlay'  icon='labeled' vertical visible width='wide'>
+            <Sidebar as={Menu} animation='overlay' icon='labeled' vertical visible width='wide'>
               <Link to="/">
-              <Menu.Item as='a'>
-                <Image src={logo} size='small' style={{marginLeft: '85px'}}></Image>
-              </Menu.Item>
-            </Link>
+                <Menu.Item as='a'>
+                  <Image src={logo} size='small' style={{ marginLeft: '85px' }}></Image>
+                </Menu.Item>
+              </Link>
               <Link to="/">
                 <Menu.Item as='a'>
                   <FontAwesomeIcon icon="home"></FontAwesomeIcon><h4>Home</h4>
@@ -63,38 +132,39 @@ class App extends React.Component {
                   <FontAwesomeIcon icon="cog"></FontAwesomeIcon><h4>Settings</h4>
                 </Menu.Item>
               </Link>
+              <Button onClick={this.createDemo}>Create Demo</Button>
             </Sidebar>
             <div>
-            <Route exact path="/" render={(props) => <Home 
-              username={this.props.username}
-              userJson={this.props.userJson}
+              <Route exact path="/" render={(props) => <Home
+                username={this.props.username}
+                userJson={this.props.userJson}
               />} />
-              <Route exact path="/marketer" render={(props) => <Marketer 
-              username={this.props.username}
-              address={this.props.address}
-              shastaMarketContract={this.props.shastaMarketContract}
-              userContract={this.props.contract}
-              web3={this.props.web3}
-              sharedMapContract={this.props.sharedMapContract}
-              ipfs={this.props.ipfs} />} />             
+              <Route exact path="/marketer" render={(props) => <Marketer
+                username={this.props.username}
+                address={this.props.address}
+                shastaMarketContract={this.props.shastaMarketContract}
+                userContract={this.props.contract}
+                web3={this.props.web3}
+                sharedMapContract={this.props.sharedMapContract}
+                ipfs={this.props.ipfs} />} />
               <Route path="/market" render={(props) => <Market
-                  username={this.props.username}
-                  ipfs={this.props.ipfs}
-                  contract={this.props.contract}
-                  address={this.props.address}
-                  web3={this.props.web3}
-                  ipfsHash={this.props.ipfsHash}
-                  ipfsFirstName={this.props.ipfsFirstName}
-                  ipfsAddress={this.props.ipfsAddress}
-                  ipfsValue={this.props.ipfsValue}
-                  shastaMarketContract={this.props.shastaMarketContract}
-                  userJson={this.props.userJson}
-                  />} />
+                username={this.props.username}
+                ipfs={this.props.ipfs}
+                contract={this.props.contract}
+                address={this.props.address}
+                web3={this.props.web3}
+                ipfsHash={this.props.ipfsHash}
+                ipfsFirstName={this.props.ipfsFirstName}
+                ipfsAddress={this.props.ipfsAddress}
+                ipfsValue={this.props.ipfsValue}
+                shastaMarketContract={this.props.shastaMarketContract}
+                userJson={this.props.userJson}
+              />} />
               <Route path="/map" render={(props) => <Map
-                  ipfs={this.props.ipfs}
-                  address={this.props.address}
-                  web3={this.props.web3}
-                  sharedMapContract={this.props.sharedMapContract}
+                ipfs={this.props.ipfs}
+                address={this.props.address}
+                web3={this.props.web3}
+                sharedMapContract={this.props.sharedMapContract}
               />} />
             </div>
           </div>
