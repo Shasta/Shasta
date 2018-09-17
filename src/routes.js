8d0f20114ebdf3b9223fddbd72436a5a904da42e
Ignasi <ignasirv@hotmail.com>
2018-09-17 11:45:42 +0200

    Finance route

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,8 @@ import Logout from './components/Logout';
 import DashboardHome from './components/Home/Home';
 import Consumer from './components/Consumer/Consumer';
 import Settings from './components/Settings/Settings';
-import Map from './components/Producer/Producer';
+import Producer from './components/Producer/Producer';
+import Finance from './components/Finance/Finance';
 
 export const publicRoutes = {
   SignUp: {
@@ -26,13 +27,17 @@ export const privateRoutes = {
     path: '/dashboard',
     component: DashboardHome,
   },
+  Finances: {
+    path: '/dashboard/finances',
+    component: Finance,
+  },
   Consumer: {
     path: '/dashboard/consumer',
     component: Consumer
   },
   Producer: {
     path: '/dashboard/Map',
-    component: Map
+    component: Producer
   },
   Settings: {
     path: '/dashboard/settings',
