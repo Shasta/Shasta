fd28539fd545601d7a6c5bff824bcfe644296471
kartojal <canillas.mail@gmail.com>
2018-09-20 12:01:12 +0200

    Glue prepaid contract confirmation. Minor component changes.

--- src/components/Consumer/stepper/MyStep.js
+++ src/components/Consumer/stepper/MyStep.js
@@ -1,98 +1,46 @@
 import React, { Component } from 'react'
-import { Step } from 'semantic-ui-react'
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+import {  Step } from 'semantic-ui-react'
+import { FontAwesomeIcon as Icon} from '@fortawesome/react-fontawesome'
+import styled from 'styled-components';
 
+const StepContent = styled(Step.Content)`
+  padding-left: 20px;
+`
 class MyStep extends Component {
 
-    render() {
-
-        switch (this.props.step) {
-
-            case 0:
-                return (
-                    <Step.Group>
-                        <Step active>
-                            <Icon icon='money-check' />
-                            <Step.Content style={{ paddingLeft: 20 }}>
-                                <Step.Title>Amount</Step.Title>
-                                <Step.Description>Choose the energy amount to buy</Step.Description>
-                            </Step.Content>
-                        </Step>
-
-                        <Step>
-                            <Icon icon='filter' />
-                            <Step.Content style={{ paddingLeft: 20 }}>
-                                <Step.Title>Filters</Step.Title>
-                                <Step.Description>Select filters (optional)</Step.Description>
-                            </Step.Content>
-                        </Step>
-
-                        <Step>
-                            <Icon icon='shopping-cart' />
-                            <Step.Content style={{ paddingLeft: 20 }}>
-                                <Step.Title>Choose offer</Step.Title>
-                            </Step.Content>
-                        </Step>
-                    </Step.Group>
-                );
-            case 1:
-                return (
-                    <Step.Group>
-                        <Step>
-                            <Icon icon='money-check' />
-                            <Step.Content style={{ paddingLeft: 20 }}>
-                                <Step.Title>Amount</Step.Title>
-                                <Step.Description>Choose the energy amount to buy</Step.Description>
-                            </Step.Content>
-                        </Step>
-
-                        <Step active>
-                            <Icon icon='filter' />
-                            <Step.Content style={{ paddingLeft: 20 }}>
-                                <Step.Title>Filters</Step.Title>
-                                <Step.Description>Select filters (optional)</Step.Description>
-                            </Step.Content>
-                        </Step>
-
-                        <Step>
-                            <Icon icon='shopping-cart' />
-                            <Step.Content style={{ paddingLeft: 20 }}>
-                                <Step.Title>Choose offer</Step.Title>
-                            </Step.Content>
-                        </Step>
-                    </Step.Group>
-                );
-            case 2:
-                return (
-                    <Step.Group>
-                        <Step>
-                            <Icon icon='money-check' />
-                            <Step.Content style={{ paddingLeft: 20 }}>
-                                <Step.Title>Amount</Step.Title>
-                                <Step.Description>Choose the energy amount to buy</Step.Description>
-                            </Step.Content>
-                        </Step>
-
-                        <Step>
-                            <Icon icon='filter' />
-                            <Step.Content style={{ paddingLeft: 20 }}>
-                                <Step.Title>Filters</Step.Title>
-                                <Step.Description>Select filters (optional)</Step.Description>
-                            </Step.Content>
-                        </Step>
-
-                        <Step active>
-                            <Icon icon='shopping-cart' />
-                            <Step.Content style={{ paddingLeft: 20 }}>
-                                <Step.Title>Choose offer</Step.Title>
-                            </Step.Content>
-                        </Step>
-                    </Step.Group>
-                );
-            default:
-                return ('')
-        }
-    }
+  render() {
+    const step = this.props.step;
+    return (
+      <Step.Group>
+        <Step active={step === 0}>
+          <Icon icon='money-check' />
+          <StepContent>
+            <Step.Title>Amount</Step.Title>
+            <Step.Description>Choose the average monthly usage</Step.Description>
+          </StepContent>
+        </Step>
+        <Step active={step === 1}>
+          <Icon icon='filter' />
+          <StepContent>
+            <Step.Title>Filters</Step.Title>
+            <Step.Description>Select filters (optional)</Step.Description>
+          </StepContent>
+        </Step>
+        <Step active={step === 2}>
+          <Icon icon='shopping-cart' />
+          <StepContent>
+            <Step.Title>Choose offer</Step.Title>
+          </StepContent>
+        </Step>
+        <Step active={step === 3}>
+          <Icon icon='file-signature' />
+          <StepContent>
+            <Step.Title>Confirm contract</Step.Title>
+          </StepContent>
+        </Step>
+      </Step.Group>
+    );
+  }
 }
 
 export default MyStep;
