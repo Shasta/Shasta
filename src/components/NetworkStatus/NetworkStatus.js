09639c8682269b9339a2add4b11732c51ffc2921
kartojal <canillas.mail@gmail.com>
2018-09-24 13:39:57 +0200

    Added NetworkStatus comp

--- src/components/NetworkStatus/NetworkStatus.js
+++ src/components/NetworkStatus/NetworkStatus.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import styled, { css } from 'styled-components';
+import { Segment } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 
 
-class ChargerMark extends Component {
+class NetworkStatus extends Component {
   render() {
-    return ""
+    const { drizzle } = this.props;
+
+    const web3 = drizzle.web3;
+    const currentNetwork = web3.eth.net.getNetworkType();
+    let segmentColor = 'red';
+
+    if (currentNetwork == 'main') {
+      segmentColor = 'green';
+    }
+
+    if (currentNetwork == 'main') {
+      segmentColor = 'yellow';
+    }
+
+    return (
+      <Segment inverted color={segmentColor}>
+        {currentNetwork}
+      </Segment>
+    )
   }
 };
 
-export default ChargerMark;
+export default NetworkStatus;
