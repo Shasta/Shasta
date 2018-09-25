0eeb6423986df28a62c8304653a67b27f873b454
Ignasi <ignasirv@hotmail.com>
2018-09-25 12:35:05 +0200

    New error message on already used organization name

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -112,7 +112,7 @@ class SignUp extends Component {
     isInstalled = _.get(drizzleState, 'web3.status', 'failed') !== 'failed';
     isLogged = isInstalled && initialized && drizzleState && Object.keys(drizzleState.accounts).length > 0;
 
-    console.log('tn', targetNetwork)
+    //console.log('tn', targetNetwork)
     const isRinkeby = isLogged && currentNetwork == targetNetwork;
 
     if (initialized) {
