67bf2f98ee6ca6134e34bd45aee3cb69d2f6472b
Ignasirv <ignasirv@hotmail.com>
2018-10-11 12:10:34 +0200

    Metamask update

--- src/app.js
+++ src/app.js
@@ -5,11 +5,11 @@ import _ from "lodash";
 import { publicRoutes, privateRoutes } from "./routes";
 import { connect } from "react-redux";
 import { UserActions } from "./redux/UserActions";
-
 import PublicLayout from "./layouts/PublicLayout";
 import PrivateLayout from "./layouts/PrivateLayout";
 
 import Error404 from "./components/404/Error404";
+var Web3 = require('web3');
 
 class App extends Component {
   constructor(props) {
@@ -24,8 +24,35 @@ class App extends Component {
 
   componentWillMount() {
     this.userActions.verify();
+
   }
 
+  componentDidMount() {
+    window.addEventListener('load', async () => {
+      // Modern dapp browsers...
+      if (window.ethereum) {
+          window.web3 = new Web3(window.ethereum);
+          try {
+              // Request account access if needed
+              await window.ethereum.enable();
+              // Acccounts now exposed
+             // web3.eth.sendTransaction({/* ... */});
+          } catch (error) {
+              // User denied account access...
+          }
+      }
+      // Legacy dapp browsers...
+      // else if (window.web3) {
+      //     window.web3 = new Web3(web3.currentProvider);
+      //     // Acccounts always exposed
+      //     web3.eth.sendTransaction({/* ... */});
+      // }
+      // Non-dapp browsers...
+      else {
+          console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+      }
+    });
+  }
   render() {
     const user = this.props.user ? this.props.user : { logged: false };
     return (
